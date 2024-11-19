6730cd96043d76b09cc4e7d69b45a16dd43ad546
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2024-11-19 15:33:03 +0530

    19 july

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [usernamee, setUsernamee] = useState("");
+  function handleUsername(e) {
+    console.log(usernamee);
+    setUsernamee(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form Submitted  Hello ", usernamee, password);
+  }
+
+  const [password, setPassword] = useState("");
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  const [gender, setGender] = useState("male");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Form Tutorial</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          {" "}
+          <label htmlFor="username"> UserName: </label>
+          <input
+            type="text"
+            id="username"
+            value={usernamee}
+            onChange={handleUsername}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password"> Password: </label>
+
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePassword}
+          />
+        </div>
+
+        <button type="submit"> Submit </button>
+      </form>
+      <h1> Radio Button</h1>
+
+      <div>
+        <label htmlFor="Male"> Male</label>
+        <input
+          type="radio"
+          checked={gender === "male"}
+          onChange={() => {
+            setGender("male");
+          }}
+        />
+      </div>
+
+      <div></div>
+      <label htmlFor="Female">Female</label>
+      <input
+        type="radio"
+        checked={gender === "female"}
+        onChange={() => {
+          setGender("female");
+        }}
+      />
     </div>
   );
 }
